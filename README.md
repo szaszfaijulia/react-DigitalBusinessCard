101738a9fbf528c9b7a883fc6cffb98a8c2fed10
szaszfaijulia <szaszfai.julia@gmail.com>
2022-04-08 12:50:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-srrpr2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-srrpr2)
---
+++ components/Info.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function Info() {
+  return(
+    <main>
+      <h1>Kép, név, gombok stb</h1>
+    </main>
+  )
+} 
