860a102472409e27f912e27ea9a7ef98057aa865
szaszfaijulia <szaszfai.julia@gmail.com>
2022-04-08 15:33:09 +0200

    1 file modified, 1 deleted

--- components/Info.js
+++ components/Info.js
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Info() {
   return(
     <main>
-      <img src="https://raw.githubusercontent.com/szaszfaijulia/react-DigitalBusinessCard/master/IMG_2938-2(igkep)%20(2).jpeg"/>
+      <img src="https://raw.githubusercontent.com/szaszfaijulia/react-DigitalBusinessCard/master/src/IMG_2938-2(igkep)3.jpeg"/>
       <h1>Júlia Szászfai</h1>
       <p>Developer</p>
-      <button type="button" className="button1"><img src="https://raw.githubusercontent.com/szaszfaijulia/react-DigitalBusinessCard/master/new-post.png"/>Email</button>
+      <button type="button" className="button1"><img src="https://raw.githubusercontent.com/szaszfaijulia/react-DigitalBusinessCard/master/src/new-post.png"/>Email</button>
     </main>
   )
 } 
